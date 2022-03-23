79246263595d2aef3a2877dc57653b902773d259
Zain Muhammad <58492711+100-100@users.noreply.github.com>
2022-03-23 00:01:38 -0400

    implement proof of work algorithm

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,8 @@
 import hashlib
 import json
-import time
+
+from time import time
+from uuid import uuid4
 
 class BlockChain(object):
     def __init__(self):
@@ -35,4 +37,16 @@ class BlockChain(object):
             'recipient': recipient,
             'amount': amount,
         })
-        return self.last_block['index'] + 1
+        return int(self.last_block['index']) + 1
+    
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+    
+    @staticmethod
+    def validate_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
