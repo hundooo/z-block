0feddc6dcc348c0177ba26efc4ce663c8687f046
Zain Muhammad <58492711+100-100@users.noreply.github.com>
2022-03-22 23:31:41 -0400

    create method to hash blocks

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,17 @@
+import hashlib
+import json
+import time
+
 class BlockChain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.new_block(previous_hash=1, proof=100)
     
     @staticmethod
     def hash(block):
-        pass
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     def new_block(self):
         pass
@@ -14,7 +20,7 @@ class BlockChain(object):
     def last_block(self):
         return self.chain[-1]
 
-    def new_transactions(self, sender, recipient, amount):
+    def new_transaction(self, sender, recipient, amount):
         self.current_transactions.append({
             'sender': sender,
             'recipient': recipient,
